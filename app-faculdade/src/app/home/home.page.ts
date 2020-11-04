ebe97becf7f4e37ee6809940ef6d089c96f185e7
kleberbenites <73402859+kleberbenites@users.noreply.github.com>
2020-11-04 03:28:14 -0300

    pagina home

--- app-faculdade/src/app/home/home.page.ts
+++ app-faculdade/src/app/home/home.page.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
+type Produto = {
+  nome: string;
+};
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
 
-  constructor() {}
+export class HomePage {
+  public produtos: Produto[] = [{ nome: '' }];
+  public novoProduto = null;
+  public addProduto() {
+
+    this.produtos.push({
+      nome: this.novoProduto,
+    }); this.novoProduto = '';
+  }
+
+  public removeProduto(toRemove: number) {
+    this.produtos.splice(toRemove, 1)
+  }
+
+  public todosProdutos() {
+    return this.produtos;
+  }
+
+
+
+  constructor() { }
 
 }
