2928a663c04517e35cc1419ec4e5b0108f9663a5
kleberbenites <73402859+kleberbenites@users.noreply.github.com>
2020-11-04 03:42:35 -0300

    criacao pagina login

--- app-faculdade/src/app/app-routing.module.ts
+++ app-faculdade/src/app/app-routing.module.ts
@@ -15,6 +15,10 @@ const routes: Routes = [
     path: 'cadastro-realizado',
     loadChildren: () => import('./cadastro-realizado/cadastro-realizado.module').then( m => m.CadastroRealizadoPageModule)
   },
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+  },
 ];
 
 @NgModule({
