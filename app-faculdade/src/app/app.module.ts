ce1df185776e786eab8b19f93a3c4bac4b2fd66c
jefaofurquim <75354180+jefaofurquim@users.noreply.github.com>
2020-12-02 13:46:48 -0300

    interface de usuario
    imports, interface, metodo de login

--- app-faculdade/src/app/app.module.ts
+++ app-faculdade/src/app/app.module.ts
@@ -9,12 +9,17 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { IonicStorageModule } from '@ionic/storage';
+import { AngularFireModule } from '@angular/fire';
+import { environment } from 'src/environments/environment';
+import {AngularFireAuthModule} from '@angular/fire/auth'
 
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, IonicStorageModule.forRoot({
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase)
+    , IonicStorageModule.forRoot({
     name: '__mydb',
     driverOrder: ['indexeddb', 'sqlite', 'websql']
   })],
--- app-faculdade/src/environments/environment.prod.ts
+++ app-faculdade/src/environments/environment.prod.ts
@@ -1,3 +1,19 @@
 export const environment = {
-  production: true
+  production: true,
+
+
+  firebase:{
+   
+    apiKey: "AIzaSyBTRHPVnYsNu7cJ8Y81u0ejbmFXs_SbAh0",
+    authDomain: "appdelistademercado-da34a.firebaseapp.com",
+    databaseURL: "https://appdelistademercado-da34a.firebaseio.com",
+    projectId: "appdelistademercado-da34a",
+    storageBucket: "appdelistademercado-da34a.appspot.com",
+    messagingSenderId: "936469921414",
+    appId: "1:936469921414:web:7335e8e305da0aea2b9b07",
+    measurementId: "G-F173WZET7X"
+  }
+  
+
+
 };
