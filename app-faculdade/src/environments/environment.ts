d9a1be47b66ef679f6da055aae4935fec224bf64
jefaofurquim <75354180+jefaofurquim@users.noreply.github.com>
2020-12-02 12:34:46 -0300

    add firebase environments
    config

--- app-faculdade/src/environments/environment.ts
+++ app-faculdade/src/environments/environment.ts
@@ -3,7 +3,25 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+
+  firebase:{
+   
+      apiKey: "AIzaSyBTRHPVnYsNu7cJ8Y81u0ejbmFXs_SbAh0",
+      authDomain: "appdelistademercado-da34a.firebaseapp.com",
+      databaseURL: "https://appdelistademercado-da34a.firebaseio.com",
+      projectId: "appdelistademercado-da34a",
+      storageBucket: "appdelistademercado-da34a.appspot.com",
+      messagingSenderId: "936469921414",
+      appId: "1:936469921414:web:7335e8e305da0aea2b9b07",
+      measurementId: "G-F173WZET7X"
+    }
+  }
+
+
+
+
+  
 };
 
 /*
